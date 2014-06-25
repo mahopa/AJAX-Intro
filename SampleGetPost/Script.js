c67a618eac8d5a3dfeb62ee3e10e50ac53ed63be
Mahopa <mahopa@gmail.com>
2014-06-25 11:12:24 -0500

    Initial

---
+++ SampleGetPost/Script.js
@@ -0,0 +1,62 @@
+﻿weatherApp = {};
+weatherApp.weathers = [];
+weatherApp.url = "https://alextestapp.firebaseio.com/.json";
+weatherApp.addWeather = function () {
+    "use strict";
+    //Post Goes Here
+    var weatherObject = {};
+    weatherObject.temp = document.getElementById("temperature").value;
+    weatherObject.city = document.getElementById("city").value;
+    var request = new XMLHttpRequest();
+    request.open("POST", weatherApp.url, true);
+    request.onload = function () {
+        if (this.status >= 200 && this.status < 400) {
+            //Call Worked. Very Success. Much Win.
+            console.log(this.response);
+            weatherApp.showWeather();
+        }
+        else {
+            //Oh Noes you got a server error
+            console.log("Error on POST: " + this.response);
+        }
+    };
+    request.onerror = function () {
+        //This is where Communication or transport error are handled
+        console.log("Communication Error on POST");
+    };
+    request.send(JSON.stringify(weatherObject));
+
+};
+weatherApp.showWeather = function () {
+    //Get Goes Here
+    console.log("show called");
+    var request = new XMLHttpRequest();
+    request.open("GET", weatherApp.url);
+    request.onload = function () {
+        if (this.status >= 200 && this.status < 400) {
+            //Do success things
+            var data = JSON.parse(this.response);
+            weatherApp.weathers = [];
+            for (var w in data) {
+                weatherApp.weathers.push(data[w]);
+            }
+            weatherApp.writeOutput();
+        }
+        else {
+            console.log("Server Errror on GET: " + this.response);
+        }
+    };
+    request.onerror = function () {
+        console.log("Errr on GET");
+    };
+    request.send();
+};
+weatherApp.writeOutput = function () {
+    //Ouputs data from the array to the output div
+    holder = "";
+    for (var w in weatherApp.weathers) {
+        holder += weatherApp.weathers[w].city + ": " + weatherApp.weathers[w].temp + "<br/>";
+    }
+    document.getElementById("output").innerHTML = holder;
+};
+weatherApp.showWeather();
