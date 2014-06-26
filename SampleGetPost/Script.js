6aa7365e2c3996316d7290d1ce8cca3a8b31c767
Mahopa <mahopa@gmail.com>
2014-06-26 13:26:26 -0500

    Abstracted AJAX

--- SampleGetPost/Script.js
+++ SampleGetPost/Script.js
@@ -1,14 +1,54 @@
 ﻿weatherApp = {};
 weatherApp.weathers = [];
-weatherApp.url = "https://alextestapp.firebaseio.com/";
+weatherApp.urlbase = "alextestapp";
+weatherApp.URLMaker = function (base, directories) {
+    var holder = "https://";
+    if (base) {
+        holder += base + ".firebaseio.com/";
+    }
+    else {
+        holder += weatherApp.urlbase + ".firebaseio.com/";
+    }
+    if (directories) {
+        holder += directories.join("/");
+    }
+    holder += ".json";
+    console.log(holder);
+    return holder;
+};
+///Our generalized AJAX CAll
+weatherApp.Ajax = function (/*Method notes?*/method, URL, data, success, failure) {
+    var request = new XMLHttpRequest();
+    request.open(method, URL);
+    request.onload = function () {
+        if (this.status >= 200 && this.status < 400) {
+            if (typeof success === 'function') {
+                success(this.response);
+            }
+        }
+        else {
+            if (typeof failure === 'function') {
+                failure();
+            }
+            else {
+                console.log("Error on " + method + " to " + URL + ": " + this.response);
+            }
+        }
+    };
+    request.onerror = function () {
+        if (typeof failure === 'function') {
+            failure();
+        }
+    };
+    request.send(data);
+};
 weatherApp.addWeather = function () {
     "use strict";
-    //Post Goes Here
     var weatherObject = {};
     weatherObject.temp = document.getElementById("temperature").value;
     weatherObject.city = document.getElementById("city").value;
     var request = new XMLHttpRequest();
-    request.open("POST", weatherApp.url, true);
+    request.open("POST", weatherApp.URLMaker(), true);
     request.onload = function () {
         if (this.status >= 200 && this.status < 400) {
             //Call Worked. Very Success. Much Win.
@@ -31,7 +71,7 @@ weatherApp.showWeather = function () {
     //Get Goes Here
     console.log("show called");
     var request = new XMLHttpRequest();
-    request.open("GET", weatherApp.url);
+    request.open("GET", weatherApp.URLMaker());
     request.onload = function () {
         if (this.status >= 200 && this.status < 400) {
             //Do success things
@@ -56,7 +96,8 @@ weatherApp.writeOutput = function () {
     //Ouputs data from the array to the output div
     holder = "";
     for (var w in weatherApp.weathers) {
-        holder += weatherApp.weathers[w].city + ": " + weatherApp.weathers[w].key + "<br/>";
+        holder += weatherApp.weathers[w].city +
+            ": <button onclick='weatherApp.Delete(\"" + weatherApp.weathers[w].key + "\")'>Delete</button><br/>";
     }
     document.getElementById("output").innerHTML = holder;
 };
@@ -64,7 +105,13 @@ weatherApp.DeleteTarget = function () {
     var urlTarget = document.getElementById("url-target").value;
     var url = weatherApp.url + urlTarget + "/.json";
     var request = new XMLHttpRequest();
-    request.open("DELETE", url);
+    request.open("DELETE", weatherApp.URLMaker(null, [urlTarget]));
     request.send();
 };
-//weatherApp.showWeather();
+weatherApp.Delete = function (urlTarget) {
+    var request = new XMLHttpRequest();
+    request.open("DELETE", weatherApp.URLMaker(null, [urlTarget]));
+    request.onload = function () { weatherApp.showWeather(); };
+    request.send();
+};
+weatherApp.showWeather();
